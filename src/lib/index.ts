cc30608e0ecb382ff38307226b2312ff1bdea2e0
tme321 <tme321@gmail.com>
2017-09-07 18:14:03 -0400

    compodoc updated and working

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,4 +5,5 @@ export * from './dropdown-menu/index';
 export * from './hamburger-menu/index';
 export * from './slideout-menu/index';
 export * from './sliding-panel/index';
+export * from './toggle/index';
 export * from './UAT.module';
