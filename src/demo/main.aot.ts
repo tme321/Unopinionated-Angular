e7977d690f4fb8ac54255c21a6fe5a93a1c2fda7
tme321 <tme321@gmail.com>
2017-09-09 15:30:42 -0400

    aot demo

--- src/demo/main.aot.ts
+++ src/demo/main.aot.ts
@@ -1,8 +1,6 @@
-/*
 import 'core-js/es7/reflect';
 import {platformBrowser} from '@angular/platform-browser';
-import {AppModuleNgFactory} from './app/ngfactory/app/app.module.ngfactory';
+import {AppModuleNgFactory} from './aot/src/demo/app/app.module.ngfactory';
 
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory)
     .catch((err: any) => console.error(err));
-*/
--- src/demo/main.ts
+++ src/demo/main.ts
@@ -1,14 +1,8 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
 import './polyfills';
-import { bootloader } from '@angularclass/hmr';
 
 function main() {
     platformBrowserDynamic().bootstrapModule(AppModule)
         .catch((err: any) => console.error(err));
 }
-
-// This function is used in conjunction with the 
-// @angularclass/hmr-loader webpack plugin to 
-// support hot module reloading
-bootloader(main);
